bfccbc8b179458920b8708bd7e87c88358ff7f25
ChanduNelavelli <112123423+ChanduNelavelli@users.noreply.github.com>
2022-10-26 23:16:24 -0500

    updated bonus done

--- routes/computation.js
+++ routes/computation.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var x = Math.random();
-var y = Math.random();
+
 var val1="Math.fround(x)  applied to "+ x + " is "+ Math.fround(x)
-var val1="Math.random(x) applied to "+ x + " is "+ Math.random(x)
+var val2="Math.random(x) applied to "+ x + " is "+ Math.random(x)
 var val3= "Math.round(x)  applied to "+ x + " is "+ Math.round(x);
 
 /* GET home page. */
